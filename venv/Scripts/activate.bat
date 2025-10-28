e3efa307b0e13f0521464994202452ee1e519586
Nexus <odllby@proton.me>
2025-10-28 20:42:00 +0000

    dashboard

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\Personnal Data\Master IFOAD\M1\S2\Projet Data Science\Données\projet_data_science\venv
+set VIRTUAL_ENV=C:\Users\Nexus\Desktop\Project_Data_Science\projet_datascience\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
